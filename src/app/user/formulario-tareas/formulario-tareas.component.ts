7b9760c5560a3241e9a8fec7e1cabb65416b56cf
facuroch <facurochh@gmail.com>
2024-11-11 18:16:42 -0300

    cambie de nombre todo lo de formulario y le agregue el html y css a la interfaz

---
+++ src/app/user/formulario-tareas/formulario-tareas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario-tareas',
+  standalone: true,
+  imports: [],
+  templateUrl: './formulario-tareas.component.html',
+  styleUrl: './formulario-tareas.component.css'
+})
+export class FormularioTareasComponent {
+
+}
