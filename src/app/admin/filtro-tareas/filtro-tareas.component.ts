b84082ed14bf4f6db48b00e87492056756e22ff2
Marcospieru <marcospieru@gmail.com>
2024-11-11 16:58:55 -0300

    Cree los dos componentes de admin

---
+++ src/app/admin/filtro-tareas/filtro-tareas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filtro-tareas',
+  standalone: true,
+  imports: [],
+  templateUrl: './filtro-tareas.component.html',
+  styleUrl: './filtro-tareas.component.css'
+})
+export class FiltroTareasComponent {
+
+}
