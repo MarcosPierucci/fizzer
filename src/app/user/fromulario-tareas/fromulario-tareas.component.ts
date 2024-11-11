f38f4d22725419770cb11d64606552912f1fc844
facurochh <facundorochh@gmail.com>
2024-11-11 17:33:06 -0300

    formulario creado

---
+++ src/app/user/fromulario-tareas/fromulario-tareas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fromulario-tareas',
+  standalone: true,
+  imports: [],
+  templateUrl: './fromulario-tareas.component.html',
+  styleUrl: './fromulario-tareas.component.css'
+})
+export class FromularioTareasComponent {
+
+}
